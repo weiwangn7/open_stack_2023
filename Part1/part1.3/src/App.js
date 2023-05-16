46c21ed8d3beb412aaed52b6f90d0d5c64b9c917
Wei <wei.wang.n7@gmail.com>
2023-05-16 14:47:53 +0200

    ex3 for part1

---
+++ Part1/part1.3/src/App.js
@@ -0,0 +1,53 @@
+const Header = props => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = props => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Content = props => {
+  return (
+    <>
+      <Part part={props.part1} />
+      <Part part={props.part2} />
+      <Part part={props.part3} />
+    </>
+  );
+};
+
+const Total = props => {
+  let total = 0;
+  total = props.part1.exercises + props.part2.exercises + props.part3.exercises;
+  return <p>Number of exercises {total}</p>;
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  };
+
+  return (
+    <>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total part1={part1} part2={part2} part3={part3} />
+    </>
+  );
+};
+
+export default App;
