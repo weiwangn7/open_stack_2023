35b5cf8777cab5aa78a7e6388ca1ce3b84df9b53
Wei <wei.wang.n7@gmail.com>
2023-05-16 14:38:24 +0200

    update ex2 of part1

--- Part1/part1.2/src/App.js
+++ Part1/part1.2/src/App.js
@@ -28,12 +28,6 @@ const Total = props => {
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
   const courseContent = [
     { part: 'Fundamentals of React', exercises: 10 },
     { part: 'Using props to pass data', exercises: 7 },
