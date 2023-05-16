5efcf3e5ae9a249b6a18fb1368c25dffb0280fb8
Wei <wei.wang.n7@gmail.com>
2023-05-16 14:08:35 +0200

    ex2 for part1

---
+++ Part1/part1.2/src/App.js
@@ -0,0 +1,52 @@
+const Header = props => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = props => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
+const Content = props => {
+  return (
+    <>
+      <Part part={props.courseContent[0].part} exercises={props.courseContent[0].exercises} />
+      <Part part={props.courseContent[1].part} exercises={props.courseContent[1].exercises} />
+      <Part part={props.courseContent[2].part} exercises={props.courseContent[2].exercises} />
+    </>
+  );
+};
+
+const Total = props => {
+  let total = 0;
+  props.courseContent.map(course => (total += course.exercises));
+  return <p>Number of exercises {total}</p>;
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+  const courseContent = [
+    { part: 'Fundamentals of React', exercises: 10 },
+    { part: 'Using props to pass data', exercises: 7 },
+    { part: 'State of a component', exercises: 14 },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content courseContent={courseContent} />
+      <Total courseContent={courseContent} />
+    </div>
+  );
+};
+
+export default App;
