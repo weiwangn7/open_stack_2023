a086939c59697da901e4e6852430c45003877eb7
Wei <wei.wang.n7@gmail.com>
2023-05-17 14:21:47 +0200

    ex13 for part 1

---
+++ Part1/part1.13/src/App.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.',
+  ];
+
+  const [vote, setVote] = useState(Array(anecdotes.length).fill(0));
+  const [selected, setSelected] = useState(0);
+  const clickHandler = () => {
+    const number = Math.floor(Math.random() * anecdotes.length);
+    setSelected(number);
+  };
+
+  const voteHandler = () => {
+    let copy = [...vote];
+    copy[selected]++;
+    setVote(copy);
+    console.log(vote[selected]);
+  };
+
+  return (
+    <>
+      <div>{anecdotes[selected]}</div>
+      <div>has {vote[selected]} votes</div>
+      <button onClick={voteHandler}>vote</button>
+      <button onClick={clickHandler}>Next anecdote</button>
+    </>
+  );
+};
+
+export default App;
